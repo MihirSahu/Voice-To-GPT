0297842e3a72b0ccb2b70242fea5a43c8a5ae9af
Mihir Sahu <46336107+MihirSahu@users.noreply.github.com>
2023-04-12 05:31:07 -0500

    Do stuff

--- README.md
+++ README.md
@@ -2,7 +2,27 @@
 
 My attempt at creating a personal voice assistant using GPT. When the system is offline, it uses [GPT4All](https://github.com/nomic-ai/gpt4all), and when it's online it uses [ChatGPT](https://chat.openai.com/).
 
+## Usage
+1. Install libraries with `pip install -r requirements.txt`
+2. Upgrade your [OpenAI account](https://openai.com/pricing) and [create an API key](https://platform.openai.com/docs/api-reference/authentication)
+3. Create an AWS account, create a new user, give it the `AmazonS3FullAccess` and `AmazonTranscribeFullAccess` permissions, and create an API key for the user
+4. Create a `.env` file and add all the following environment variables
+5. Run the script with `python main.py`
+
+## Environment Variables (.env)
+- OPENAI_API_KEY
+- MODEL_ENGINE
+- VOICE_RATE
+- MAX_TOKENS
+- AWS_ACCESS_KEY_ID
+- AWS_SECRET_ACCESS_KEY
+- S3_BUCKET_NAME
+- REGION_NAME
+- SAMPLING_FREQUENCY
+- CHANNELS
+
 ## Resources
 - [GPT4All](https://github.com/nomic-ai/gpt4all)
 - [ChatGPT](https://chat.openai.com/)
 - [Integrate ChatGPT with Voice to Text](https://betterprogramming.pub/how-to-integrate-chatgpt-with-voice-to-text-with-python-40300b8a77d1)
+- [Boto3 Documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/index.html)
--- main.py
+++ main.py
@@ -1,18 +1,83 @@
 import os
 import openai
 from dotenv import load_dotenv
+import pyttsx3
+import boto3
+import sounddevice as sd
+from scipy.io.wavfile import write
+import datetime
+import urllib
+import json
+import time as tm
 
+# Load and get environment variables
 load_dotenv()
-
+## OpenAI environment variables
 openai.api_key = os.getenv("OPENAI_API_KEY")
 model_engine = os.getenv("MODEL_ENGINE")
-prompt = input("Enter your prompt: ")
-max_tokens = 50
+max_tokens = int(os.getenv("MAX_TOKENS"))
+## Speech-to-text environment variables
+sampling_frequency = int(os.getenv("SAMPLING_FREQUENCY"))
+channels = int(os.getenv("CHANNELS"))
+## AWS environment variables
+aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
+aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+s3_bucket_name = os.getenv("S3_BUCKET_NAME")
+region_name = os.getenv("REGION_NAME")
+## Text-to-speech environment variables
+voice_rate = os.getenv("VOICE_RATE")
 
+# Record audio
+recording = sd.rec(int(5 * sampling_frequency), samplerate=sampling_frequency, channels=channels)
+sd.wait()
+# Save the recorded audio
+time = datetime.datetime.now().strftime("%m-%d-%Y %H-%M-%S")
+write(f"{time}.wav", sampling_frequency, recording)
+
+# Upload audio to S3
+s3_client = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+with open(f"{time}.wav", "rb") as f:
+    s3_client.upload_fileobj(f, s3_bucket_name, f"{time}.wav")
+
+# Create a client for AWS Transcribe
+transcribe_client = boto3.client('transcribe', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, region_name=region_name)
+# Use the client to create a transcription job
+transcription_job_name = datetime.datetime.now().strftime("%m-%d-%Y-%H-%M-%S")
+response = transcribe_client.start_transcription_job(
+    TranscriptionJobName=f'{transcription_job_name}',
+    Media={'MediaFileUri': f's3://{s3_bucket_name}/{time}.wav'},
+    MediaFormat='wav',
+    LanguageCode='en-US'
+)
+# Wait for the job to finish
+while True:
+    status = transcribe_client.get_transcription_job(TranscriptionJobName=f'{transcription_job_name}')['TranscriptionJob']['TranscriptionJobStatus']
+    if status in ['COMPLETED', 'FAILED']:
+        break
+    print("Job still in progress...")
+    tm.sleep(3)
+# Get the transcription results
+if status == 'COMPLETED':
+    response = transcribe_client.get_transcription_job(TranscriptionJobName=f'{transcription_job_name}')
+    transcript_uri = response['TranscriptionJob']['Transcript']['TranscriptFileUri']
+    transcript_response = urllib.request.urlopen(transcript_uri)
+    transcript_data = transcript_response.read()
+    transcript_text = json.loads(transcript_data)['results']['transcripts'][0]['transcript']
+
+# Get response from OpenAI
 response = openai.ChatCompletion.create(
     model=model_engine,
-    messages=[{"content": prompt, "role": "user"}],
+    messages=[{"content": transcript_text, "role": "user"}],
     max_tokens=max_tokens,
 )
+message = response.choices[0].message.content
 
-print(response.choices[0].message.content)
+# Initialize text-to-speech engine
+engine = pyttsx3.init()
+engine.setProperty('rate', voice_rate)
+engine.setProperty('voices', engine.getProperty('voices')[0].id)
+
+# Print and speak response
+print(message)
+engine.say(message)
+engine.runAndWait()
